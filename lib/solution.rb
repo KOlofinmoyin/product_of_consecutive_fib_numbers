5cfb992257fa4a611468b401faa2800389a32c68
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-03-06 22:21:12 +0000

    takes 1 and returns [0,1,true] .

---
+++ lib/solution.rb
@@ -0,0 +1,5 @@
+class Solution
+  def self.resolve(number)
+    [0,1,true]
+  end
+end
---
+++ spec/solution_spec.rb
@@ -0,0 +1,7 @@
+require 'solution'
+
+describe Solution do
+  it "takes 1 and returns [0,1,true]" do
+    expect(Solution.resolve(1)).to eq [0,1,true]
+  end
+end
