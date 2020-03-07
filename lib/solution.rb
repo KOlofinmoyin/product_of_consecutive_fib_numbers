8c98cbafc259bb2a2a723b0ade65c7687dbd842b
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-03-07 11:10:03 +0000

    takes 800 and returns [34, 55, false]

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,20 +1,35 @@
 class Solution
   def self.resolve(number)
     return [0,1,number == 1] if number < 2
-
+    
+    fib_nums = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55]
     closest_nums = [0, 1];
-    fib_nums = [0, 1, 2, 3, 5, 8, 13, 21, 34]
+    last_nums = false;
+
     fib_nums[0..-2].each_with_index { |num, index|
+      new_pair = [num, fib_nums[index + 1]]
+
       if num * fib_nums[index + 1] == number
-        return [num, fib_nums[index + 1], true]
-      elsif (closest_nums.inject(&:*) - number).abs > ([num, fib_nums[index + 1]].inject(&:*) - number).abs
-        closest_nums = [num, fib_nums[index + 1]];
+        return new_pair.push true
+      elsif self.new_pair_is_closer?(closest_nums, new_pair, number)
+        closest_nums = new_pair;
       end
+
+      last_nums = new_pair
+      break if self.new_pair_is_further?(closest_nums, new_pair, number) 
     }
 
-    return closest_nums.push(false)
-    # number == 6 ? [2,3,true] :
-    #   number == 16 ? [3,5,false] :
-    #   [0,1,number == 1]
+    closest_product = closest_nums.inject(&:*)
+    last_product = last_nums.inject(&:*)
+
+    closest_product < last_product ? last_nums.push(false) : closest_nums.push(false)
+  end
+
+  def self.new_pair_is_closer? closest, last, number
+    (closest.inject(&:*) - number).abs > (last.inject(&:*) - number).abs
+  end
+
+  def self.new_pair_is_further? closest, last, number
+    (closest.inject(&:*) - number).abs < (last.inject(&:*) - number).abs
   end
 end
--- spec/solution_spec.rb
+++ spec/solution_spec.rb
@@ -13,8 +13,8 @@ describe Solution do
     expect(Solution.resolve(6)).to eq [2,3,true]
   end
 
-  it "takes 16 and returns [3,5,false]" do
-    expect(Solution.resolve(16)).to eq [3,5,false]
+  it "takes 16 and returns [5,8,false]" do
+    expect(Solution.resolve(16)).to eq [5,8,false]
   end
 
   it "takes 104 and returns [8,13, true]" do
@@ -23,5 +23,9 @@ describe Solution do
 
   it "takes 714 and returns [21, 34, true]" do
     expect(Solution.resolve(714)).to eq [21, 34, true]
+  end
+
+  it "takes 800 and returns [34, 55, false]" do
+    expect(Solution.resolve(800)).to eq [34, 55, false]
   end
 end
